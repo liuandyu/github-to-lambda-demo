4d55d7ae2b678bd0bce38919aa21919fb881eafe
Jeff Yu <jyu@yamaha.com>
2022-11-14 15:20:20 -0800

    update code

---
+++ index.ts
@@ -0,0 +1,14 @@
+import {APIGatewayProxyEvent, APIGatewayProxyCallbackV2, Handler} from "aws-lambda";
+import * as _ from 'lodash';
+
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+    const max = 10;
+    const val = _.random(max);
+
+    const response = {
+        statusCode: 200,
+        body: `The random value (max ${max}) is ${val}`
+    };
+    return response;
+};
